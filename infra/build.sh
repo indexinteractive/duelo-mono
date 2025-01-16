e7c14fb7c29e11e557e95cda1cc302f23572948d
Felipe Mullen <felipedmullen@gmail.com>
2025-01-15 20:20:17 -0800

    Add multiplayer configuration

--- infra/build.sh
+++ infra/build.sh
@@ -1,8 +1,20 @@
 UNITY_PATH="$HOME/Unity/Hub/Editor/6000.1.0a9/Editor/Unity"
 PROJECT_PATH="$HOME/ind3x/duelo-mono/duelo-unity"
 BUILDS_ROOT="$PROJECT_PATH/Builds"
+APP_NAME=duelo-server
+DOCKER_TAG=latest
 
-echo "Building server from $PROJECT_PATH"
+echo ""
+echo "DUELO build script"
+echo "------------------"
+echo ""
+echo "App name: $APP_NAME"
+echo "Unity path: $UNITY_PATH"
+echo "Project path: $PROJECT_PATH"
+echo "Build output path: $BUILDS_ROOT"
+echo ""
+
+echo "Running Unity script"
 
 $UNITY_PATH \
     -batchmode \
@@ -11,19 +23,24 @@ $UNITY_PATH \
     -projectPath "$PROJECT_PATH" \
     -executeMethod Duelo.Build.BuildScript.BuildServer \
     -logFile "build.log" \
-    --buildFolder "$BUILDS_ROOT/server_linux" \
-    --appName "duelo-server" \
+    --buildFolder "$BUILDS_ROOT/server_standalone" \
+    --appName "$APP_NAME"
 
-if [ $? -ne 0 ]; then
-    echo "Build failed. Check the log file for details: $(pwd)/build.log"
-else
-    echo "Build finished successfully"
+if grep -q "Build Finished, Result: Success." build.log; then
+    echo "Unity build finished successfully"
     echo "Logs: $(pwd)/build.log"
     echo "Builds: $PROJECT_PATH/Builds"
+else
+    echo "Build failed. Check the log file for details:"
+    echo "$(pwd)/build.log"
+    exit 1
 fi
 
 cp Dockerfile $BUILDS_ROOT
 cp server.json $BUILDS_ROOT
 cd $BUILDS_ROOT
 
-docker build -t duelo-server:latest .
+DOCKER_LABEL="$APP_NAME:$DOCKER_TAG"
+
+echo "Building Docker image: $DOCKER_LABEL"
+docker build -t $DOCKER_LABEL .

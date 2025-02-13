a255a9c0198cf93f7d1d06eca4ca44229feee741
Felipe Mullen <felipedmullen@gmail.com>
2025-02-12 23:00:31 -0800

    Implement a HeartContainer class

---
+++ duelo-unity/Assets/_duelo/02_scripts/client/ui/hud/HeartContainer.cs
@@ -0,0 +1,46 @@
+namespace Duelo.Client.UI
+{
+    using UnityEngine;
+    using UnityEngine.UI;
+    using UnityEngine.Assertions;
+
+    public class HeartContainer : MonoBehaviour
+    {
+        public enum HeartState
+        {
+            Full,
+            Half,
+            Empty
+        }
+
+        #region State
+        public HeartState State;
+        #endregion
+
+        #region References
+        public Image FullHeart;
+        public Image HalfHeart;
+        public Image EmptyHeart;
+        #endregion
+
+        #region Unity Lifecycle
+        private void Awake()
+        {
+            Assert.IsNotNull(FullHeart, $"[HeartContainer] {name}: FullHeart is not assigned");
+            Assert.IsNotNull(HalfHeart, $"[HeartContainer] {name}: HalfHeart is not assigned");
+            Assert.IsNotNull(EmptyHeart, $"[HeartContainer] {name}: EmptyHeart is not assigned");
+
+            UpdateHeartDisplay(State);
+        }
+        #endregion
+
+        public void UpdateHeartDisplay(HeartState state)
+        {
+            State = state;
+
+            FullHeart.gameObject.SetActive(state == HeartState.Full);
+            HalfHeart.gameObject.SetActive(state == HeartState.Half);
+            EmptyHeart.gameObject.SetActive(state == HeartState.Empty);
+        }
+    }
+}

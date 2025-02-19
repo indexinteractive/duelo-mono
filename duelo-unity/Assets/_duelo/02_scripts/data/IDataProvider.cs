9ab4d67558cf99876d1f499b1b182943991e092d
Felipe Mullen <felipedmullen@gmail.com>
2025-02-18 19:41:37 -0800

    Implement a subscription system for a data provider that will be used to replace / supersede firebase

---
+++ duelo-unity/Assets/_duelo/02_scripts/data/IDataProvider.cs
@@ -0,0 +1,14 @@
+namespace Duelo.Data
+{
+    using System;
+    using System.Collections;
+    using System.Linq.Expressions;
+    using Duelo.Common.Model;
+
+    public interface IDataProvider
+    {
+        public IEnumerator Initialize(MatchDto match);
+        public void Subscribe<TProp>(Expression<Func<MatchDto, TProp>> propertyExpression, Action<TProp, TProp> callback);
+        public void Set<TProp>(Expression<Func<MatchDto, TProp>> propertyExpression, TProp value);
+    }
+}
